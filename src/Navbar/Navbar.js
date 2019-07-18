5784a3f014957122b15a05f38ddb0296c2c8eecb
isoon <phirachat.k@ku.th>
2019-07-18 09:20:24 +0700

    change navbar to float left top link button

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,48 +1,15 @@
 import React, { useState } from 'react'
-import { Menu, Icon } from 'antd';
-
-const { SubMenu } = Menu;
+import { Button } from 'antd';
 
 
 const Navbar = () => {
 
-    const [current, setCurrent] = useState("home")
-
-    const handleClick = (e) => setCurrent(e.key)
-
     return (
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-            <Menu.Item key="Home">
-                <Icon type="home" />
-                Navigation One
-            </Menu.Item>
-            <Menu.Item key="app" disabled>
-                <Icon type="appstore" />
-                Navigation Two
-            </Menu.Item>
-            <SubMenu
-                title={
-                    <span className="submenu-title-wrapper">
-                        <Icon type="setting" />
-                        Navigation Three - Submenu
-                    </span>
-                }
-            >
-                <Menu.ItemGroup title="Item 1">
-                    <Menu.Item key="setting:1">Option 1</Menu.Item>
-                    <Menu.Item key="setting:2">Option 2</Menu.Item>
-                </Menu.ItemGroup>
-                <Menu.ItemGroup title="Item 2">
-                    <Menu.Item key="setting:3">Option 3</Menu.Item>
-                    <Menu.Item key="setting:4">Option 4</Menu.Item>
-                </Menu.ItemGroup>
-            </SubMenu>
-            <Menu.Item key="alipay">
-                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                    Navigation Four - Link
-                </a>
-            </Menu.Item>
-        </Menu>
+        <div style={{display: "flex", flexDirection: "column", marginRight: "auto", width: "100px"}}>
+            <Button type="link">home</Button>
+            <Button type="link">gallery</Button>
+            <Button type="link">contact</Button>
+        </div>
     )
 }
 
